88ec662601952a214e6ea804933ebcc0921bb1df
aidensV <nameblacllis@gmail.com>
2020-03-03 13:03:26 +0700

    edit dashboard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        config(['app.locale' => 'id']);
+    \Carbon\Carbon::setLocale('id');
+    \Carbon\Carbon::setLocale(config('app.locale'));
+
+
     }
 }
